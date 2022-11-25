aa32cbb3abcd32e9d8d766c8ed69ed57d20d31fb
Iñaki Rosello <inarocky56@gmail.com>
2022-11-24 21:38:12 -0300

    Agrego power

--- Codigos Python/Data Science/DataScience.py
+++ Codigos Python/Data Science/DataScience.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('./Data Science/celebrity_deaths_2016.csv', header=None)
+df = pd.read_csv('celebrity_deaths_2016.csv', header=None)
 pd.set_option("display.max.columns", None)
 
 df = df.drop([6, 7],axis=1)
@@ -32,7 +32,7 @@ for y in afterSortCodDict.keys():
     temp += 1    
 
 print(temp)
-afterSortCodDict = afterSortCodDict[295:308]
+#afterSortCodDict = afterSortCodDict[295:308]
 x2 = afterSortCodDict.keys()
 y2 = afterSortCodDict.values() 
 plt.bar(x2,y2, label='barra', color="blue")
