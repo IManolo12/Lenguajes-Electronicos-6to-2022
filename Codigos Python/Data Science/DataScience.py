f218c566ff8fb01adaa1a1e32042b895e4125031
Iñaki Rosello <inarocky56@gmail.com>
2022-11-24 21:51:23 -0300

    Agregué comentarios

--- Codigos Python/Data Science/DataScience.py
+++ Codigos Python/Data Science/DataScience.py
@@ -1,10 +1,13 @@
+#Incluyo las bibliotecas
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+#Guardo el dataset con un método de pandas en la variable df
 df = pd.read_csv('celebrity_deaths_2016.csv', header=None)
 pd.set_option("display.max.columns", None)
 
+#Elimino 2 columnas innecesarias y remplazo valores nulos por cero
 df = df.drop([6, 7],axis=1)
 df = df.fillna(0)
 
@@ -13,25 +16,33 @@ codDict = {}
 # for i in df.values:
 #     if cod != 0:
 #         codDic = {}
+
+#Guardo las enfermedades en una lista sin que se guarden 2 veces seguidas. También como hay varios tipos de cancer agrupé todos en uno
 for i in df.values:
     age = i[3]
     cod = i[5]
     if cod != 0 and cod not in codList:
         if "cancer" not in cod: codList.append(cod)
 
+#Las keys del diccionario van a tener los strings que se encuentren en la lista
 codDict = {k: 0 for k in codList}
 
+#Si se repite una enfermedad se le suma 1 al valor de esa enfermedad
 for i in df.values:
     cod = i[5]
     for k in codDict.keys():
         if k == cod: codDict[k] += 1
 
+#Ordeno las keys de menor a mayor teniendo en cuenta su valor
 afterSortCodDict = dict(zip(codList, sorted(codDict.values())))
 temp = 0
 for y in afterSortCodDict.keys(): 
     temp += 1    
 
 print(temp)
+
+
+#Gráfico----Falta lograr mostrar un slice del diccionario de las keys con los mayores 5 valores
 #afterSortCodDict = afterSortCodDict[295:308]
 x2 = afterSortCodDict.keys()
 y2 = afterSortCodDict.values() 
